70a7c599f9aba73c934ba0551bc686652b3c1905
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2019-09-30 15:33:26 +0400

    day 1 commit

--- 1-UI/index.js
+++ 1-UI/index.js
@@ -20,21 +20,29 @@ showBtn.addEventListener('click', e => {
 })
 
 
-
-
-
-
-
-
 //----------------------------------------------
 // using DOM + XHR ( XML HttpReaquest ) API
 //----------------------------------------------
 
 var todosBtn = document.getElementById('todos-btn')
+var todosCountListEle = document.getElementById('todos-count-list')
+var todosCountInpEle = document.getElementById('todos-count-inp')
 todosBtn.addEventListener('click', e => {
+    loadAndRenderTodos(5)
+})
+todosCountListEle.addEventListener('change', e => {
+    if (e.target.value)
+        loadAndRenderTodos(e.target.value)
+})
+todosCountInpEle.addEventListener('blur', e => {
+    if (e.target.value)
+        loadAndRenderTodosV2(e.target.value)
+})
 
+
+function loadAndRenderTodos(count) {
     var xhr = new XMLHttpRequest();
-    var url = `https://jsonplaceholder.typicode.com/todos?_limit=${5}`
+    var url = `https://jsonplaceholder.typicode.com/todos?_limit=${count}`
     xhr.open('GET', url)
     xhr.send();
     xhr.onreadystatechange = function () {
@@ -43,7 +51,7 @@ todosBtn.addEventListener('click', e => {
             var todos = JSON.parse(text);
             var liElements = todos.map((todo) => {
                 let liEle = `
-                    <li class="list-group-item ${todo.completed?'bg-info':''}">
+                    <li class="list-group-item ${todo.completed ? 'bg-info' : ''}">
                         <span class="badge badge-dark">${todo.id}</span>
                         <span>${todo.title}</span>
                         <span class="badge badge-danger">${todo.completed}</span>
@@ -56,6 +64,40 @@ todosBtn.addEventListener('click', e => {
         }
     }
     document.getElementById('progress').innerHTML = "loading todos, please wait.."
+}
 
 
-})
+
+function loadAndRenderTodosV2(count) {
+    var url = `https://jsonplaceholder.typicode.com/todos?_limit=${count}`
+    let promise = fetch(url);
+    promise
+        .then(response => response.json())
+        .then(todos => {
+            var liElements = todos.map((todo) => {
+                let liEle = `
+                <li class="list-group-item ${todo.completed ? 'bg-info' : ''}">
+                    <span class="badge badge-dark">${todo.id}</span>
+                    <span>${todo.title}</span>
+                    <span class="badge badge-danger">${todo.completed}</span>
+                </li>
+            `
+                return liEle;
+            })
+            document.getElementById('progress').innerHTML = ""
+            document.getElementById('todos').innerHTML = liElements.join(" ")
+        });
+    document.getElementById('progress').innerHTML = "loading todos, please wait.."
+}
+
+
+
+
+//----------------------------------------------
+// using DOM + Time API
+//----------------------------------------------
+
+let timeEle=document.getElementById('time');
+setInterval(() => {
+    timeEle.innerHTML = new Date().toLocaleTimeString('en-US', { timeZone: 'Asia/Dubai' })
+}, 1000);
