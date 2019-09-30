d421effea07b92347d472a1f81b8e690373fc82d
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2019-09-30 14:36:23 +0400

    ui intro

--- 1-UI/index.js
+++ 1-UI/index.js
@@ -5,9 +5,57 @@ console.log("-index.js-");
 //----------------------------------------------
 
 var box = document.getElementsByClassName('alert')[0];
-var nextBtn = document.querySelector('.btn');
+var nextBtn = document.querySelector('.btn-dark');
+var hideBtn = document.querySelector('.btn-danger');
+var showBtn = document.querySelector('.btn-primary');
 
 nextBtn.addEventListener('click', e => {
     box.innerHTML = "learn UI tech"
 })
+hideBtn.addEventListener('click', e => {
+    box.style.display = 'none'
+})
+showBtn.addEventListener('click', e => {
+    box.style.display = ''
+})
 
+
+
+
+
+
+
+
+//----------------------------------------------
+// using DOM + XHR ( XML HttpReaquest ) API
+//----------------------------------------------
+
+var todosBtn = document.getElementById('todos-btn')
+todosBtn.addEventListener('click', e => {
+
+    var xhr = new XMLHttpRequest();
+    var url = `https://jsonplaceholder.typicode.com/todos?_limit=${5}`
+    xhr.open('GET', url)
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            var text = xhr.responseText;
+            var todos = JSON.parse(text);
+            var liElements = todos.map((todo) => {
+                let liEle = `
+                    <li class="list-group-item ${todo.completed?'bg-info':''}">
+                        <span class="badge badge-dark">${todo.id}</span>
+                        <span>${todo.title}</span>
+                        <span class="badge badge-danger">${todo.completed}</span>
+                    </li>
+                `
+                return liEle;
+            })
+            document.getElementById('progress').innerHTML = ""
+            document.getElementById('todos').innerHTML = liElements.join(" ")
+        }
+    }
+    document.getElementById('progress').innerHTML = "loading todos, please wait.."
+
+
+})
