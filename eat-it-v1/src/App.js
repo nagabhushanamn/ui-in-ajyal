a9354e6cabd087567bd261f3fbce59341edcf62e
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2019-10-01 14:21:27 +0400

    items

--- eat-it-v1/src/App.js
+++ eat-it-v1/src/App.js
@@ -4,17 +4,44 @@ import 'bootstrap/dist/css/bootstrap.css';
 class App extends React.Component {
 
   state = {
-    item: {
-      id: 1,
-      name: 'Veg',
-      price: 34,
-      imgPath: 'images/veg.png',
-      description: 'veg is yummy'
-    }
+    items: [
+      {
+        id: 1,
+        name: 'Veg',
+        price: 120.00,
+        imgPath: 'images/veg.png',
+        description: 'veg is yummy'
+      },
+      {
+        id: 2,
+        name: 'Non-Veg',
+        price: 150.00,
+        imgPath: 'images/non-veg.png',
+        description: 'non-veg is yummy, not healthy always'
+      },
+    ]
   }
-
+  renderItems(items) {
+    return items.map(item => {
+      return (
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-sm-3 col-md-3">
+              <img src={item.imgPath} className="img-fluid" />
+            </div>
+            <div className="col-sm-9 col-md-9">
+              <h5>{item.name}</h5>
+              <h6><span>&#x62f;&#x2e;&#x625;&nbsp;</span>{item.price}</h6>
+              <div>{item.description}</div>
+              <button className="btn btn-dark btn-sm">buy</button>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
   render() {
-    let item = this.state.item;
+    let items = this.state.items;
     return (
       <div>
         <nav className="navbar navbar-light bg-info">
@@ -23,19 +50,7 @@ class App extends React.Component {
         <hr />
         <hr />
         <div className="container">
-          <div className="list-group-item">
-            <div className="row">
-              <div className="col-sm-3 col-md-3">
-                <img src={item.imgPath} className="img-fluid" />
-              </div>
-              <div className="col-sm-9 col-md-9">
-                <h5>{item.name}</h5>
-                <h6>{item.price}-AED  </h6>
-                <div>{item.description}</div>
-                <button className="btn btn-dark btn-sm">buy</button>
-              </div>
-            </div>
-          </div>
+          {this.renderItems(items)}
         </div>
 
       </div>
